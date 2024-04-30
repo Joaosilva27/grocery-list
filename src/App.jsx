2a918205054aedbebd21c9b130311d655812731d
Joaosilva27 <joaosilva7875@gmail.com>
2024-04-30 10:58:19 +0200

    local storage & css

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MinusCartIcon from "./Icons/cart-minus.svg";
 
 function App() {
-  const [imageSearch, setImageSearch] = useState();
-  const [imageSearchList, setImageSearchList] = useState([]);
-  const [imageResults, setImageResults] = useState([]);
+  const [imageSearch, setImageSearch] = useState("");
+  const [imageSearchList, setImageSearchList] = useState(() => {
+    const storedList = localStorage.getItem("imageSearchList");
+    return storedList ? JSON.parse(storedList) : [];
+  });
+  const [imageResults, setImageResults] = useState(() => {
+    const storedResults = localStorage.getItem("imageResults");
+    return storedResults ? JSON.parse(storedResults) : [];
+  });
 
   const onHandleSearch = async e => {
     e.preventDefault();
 
-    let data = JSON.stringify({
-      q: `supermarket ${imageSearch}`,
-      num: 1,
-    });
+    if (imageSearch) {
+      let data = JSON.stringify({
+        q: `supermarket ${imageSearch}`,
+        num: 1,
+      });
 
-    let config = {
-      method: "post",
-      url: "https://google.serper.dev/images",
-      headers: {
-        "X-API-KEY": "1f75c2b111ad0bd95a563938e5cb0d1cdb8add15",
-        "Content-Type": "application/json",
-      },
-      data: data,
-    };
+      let config = {
+        method: "post",
+        url: "https://google.serper.dev/images",
+        headers: {
+          "X-API-KEY": "1f75c2b111ad0bd95a563938e5cb0d1cdb8add15",
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
 
-    try {
-      const response = await axios(config);
-      console.log(JSON.stringify(response.data));
+      try {
+        const response = await axios(config);
+        console.log(JSON.stringify(response.data));
 
-      setImageSearchList(prevState => [imageSearch[0].toUpperCase() + imageSearch.substring(1), ...prevState]);
+        setImageSearchList(prevState => [imageSearch[0].toUpperCase() + imageSearch.substring(1), ...prevState]);
 
-      setImageResults(prevState => [...response.data.images, ...prevState]);
+        setImageResults(prevState => [...response.data.images, ...prevState]);
 
-      setImageSearch("");
-    } catch (error) {
-      console.log(error);
+        setImageSearch("");
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return;
     }
   };
 
@@ -68,6 +78,11 @@ function App() {
     </div>
   );
 
+  useEffect(() => {
+    localStorage.setItem("imageSearchList", JSON.stringify(imageSearchList));
+    localStorage.setItem("imageResults", JSON.stringify(imageResults));
+  }, [imageSearchList, imageResults]);
+
   return (
     <div className='flex justify-center items-center m-10 flex-col'>
       <div className='flex mb-10'>
@@ -78,11 +93,13 @@ function App() {
             value={imageSearch}
             className='text-black bg-white rounded-full outline-none pl-6'
           ></input>
-          <button className='ml-4 mr-2' onClick={onHandleSearch}>
+          <button className='bg-blue-500 ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl text-sm' onClick={onHandleSearch}>
             Add
           </button>
         </form>
-        <button onClick={onHandleClearList}>Clear List</button>
+        <button className='bg-blue-500 ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl text-sm' onClick={onHandleClearList}>
+          Clear List
+        </button>
       </div>
       {imageResults.map((image, index) => (
         <GroceryCard item={imageSearchList[index]} image={image.imageUrl} index={index} key={index} />
